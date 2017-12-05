3ea7d9c04bd4521613e2953316b9e118b3ddc101
artemkopytko <kopytkoartem@gmail.com>
2017-12-05 17:57:34 +0200

    Initial commit

---
+++ Cafes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cafes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
