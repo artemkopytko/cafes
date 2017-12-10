26e6cc3528de77dde8a6588841b0de2fd9b1c031
artemkopytko <kopytkoartem@gmail.com>
2017-12-10 16:49:35 +0200

    Program description

--- Cafes/Program.cs
+++ Cafes/Program.cs
@@ -10,6 +10,16 @@ namespace Cafes
             Cafe italianCafe = new ItalianCafe();
             Cafe asianCafe = new AsianCafe();
 
+            /* TODO : 
+             * Логика клиента.
+             * Выбираем кафе.
+             * Выбираем блюдо.
+             * Возвращаемся к меню.
+             * Выбираем блюдо.
+             * Возвращаемся к меню.
+             * Возвращаемся к выбору кафе.
+             * Завершаем программу.
+             */
             italianCafe.GetDish("Minestrone");
             Console.WriteLine("\n");
             italianCafe.GetDish("Pizza");
